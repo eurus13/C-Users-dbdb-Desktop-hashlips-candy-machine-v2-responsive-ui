0467042b154528f9c6ebc9145516bf63d33c6b40
eurus13 <jacewestwood@gmail.com>
2022-01-14 21:57:24 -0600

    Update Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -463,8 +463,8 @@ const Home = (props: HomeProps) => {
                             <h2>My NFT</h2>
                             <br/>
                             <div><Price label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " SOL") : (price + " SOL")}/><Image
-                                src="cool-cats.gif"
-                                alt="NFT To Mint"/></div>
+                                src="sunset.jpg"
+                                alt="MetaGhoul To Mint"/></div>
                             <br/>
                             {wallet && isActive && whitelistEnabled && (whitelistTokenBalance > 0) &&
                               <h3>You have {whitelistTokenBalance} whitelist mint(s) remaining.</h3>}
